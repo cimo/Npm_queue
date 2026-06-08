f26a2f811d079588d21edc7ee96185e7c73cda56
cimo <simone.dagostino@gmail.com>
2026-06-08 20:25:50 +0900

    update

--- src/Service.ts
+++ src/Service.ts
@@ -4,22 +4,22 @@ import * as model from "./Model.js";
 let serialIsBusy = false;
 let parallelCountRunning = 0;
 
-export const list: model.Ijob[] = [];
+export const jobList: model.Ijob[] = [];
 
 export const processSerial = (): void => {
     if (serialIsBusy) {
         return;
     }
 
-    const job = list.shift();
+    const jobShift = jobList.shift();
 
-    if (!job) {
+    if (!jobShift) {
         return;
     }
 
     serialIsBusy = true;
 
-    job().finally(() => {
+    jobShift().finally(() => {
         serialIsBusy = false;
 
         processSerial();
@@ -28,15 +28,15 @@ export const processSerial = (): void => {
 
 export const processParallel = (parralelMax: number): void => {
     while (parallelCountRunning < parralelMax) {
-        const job = list.shift();
+        const jobShift = jobList.shift();
 
-        if (!job) {
+        if (!jobShift) {
             return;
         }
 
         parallelCountRunning++;
 
-        job().finally(() => {
+        jobShift().finally(() => {
             parallelCountRunning--;
 
             processParallel(parralelMax);
